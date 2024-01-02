253deb27d07340a30c38693c5b3dff82045eca2d
Kenechukwu Akubue <kengoon19@gmail.com>
2024-01-02 22:16:19 +0100

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for g in range(0, 9):
     for f in range(g + 1, 10):
         if g != 8:
-            print("{}{}".format(g, f, end=", "))
+            print("{}{}".format(g, f), end=", ")
         else:
             print("{}{}".format(g, f))
