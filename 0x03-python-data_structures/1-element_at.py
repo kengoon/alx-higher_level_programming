3824fff0610c809f0652bb030e4ff3dff91506f1
Kenechukwu Akubue <kengoon19@gmail.com>
2024-01-05 07:55:11 +0100

    4-new_in_list.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return
-    return my_list[idx]
+    if 0 <= idx < len(my_list):
+        return my_list[idx]
