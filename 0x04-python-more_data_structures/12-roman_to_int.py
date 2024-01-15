9ae6eaccc1f9d4266b1de45164d5a22d182d7ad0
Kenechukwu Akubue <kengoon19@gmail.com>
2024-01-15 20:31:27 +0100

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
 
     roman_dict = {
             "I": 1,
@@ -15,13 +14,13 @@ def roman_to_int(roman_string):
     }
     num = 0
 
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
+    for i, s in enumerate(roman_string):
+        if not roman_dict.get(s) or roman_string.count(s) > 3:
+            return 0
 
-        if (i != (len(roman_string) - 1) and
-            roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-            num += roman_dict[roman_string[i]] * -1
+        if (i < len(roman_string) - 1 and
+                roman_dict[s] < roman_dict[roman_string[i + 1]]):
+            num += roman_dict[s] * -1
         else:
-            num += roman_dict[roman_string[i]]
+            num += roman_dict[s]
     return num
