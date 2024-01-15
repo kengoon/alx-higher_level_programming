e45a34ada5e672929f9b24f396fec9190317419d
Kenechukwu Akubue <kengoon19@gmail.com>
2024-01-15 21:44:32 +0100

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not isinstance(my_list, list) or not my_list:
+        return 0
+
+    avg = 0
+    size = 0
+    for a, b in my_list:
+        avg += a * b
+        size += b
+    return avg / size
