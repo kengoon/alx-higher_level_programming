1377b60778d71d6b76b0e5821bc8165b2b73f670
Kenechukwu Akubue <kengoon19@gmail.com>
2024-01-24 08:25:15 +0100

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
             ret += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
-            break
     print("")
     return (ret)
