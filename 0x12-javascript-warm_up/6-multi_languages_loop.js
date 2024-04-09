3fc1cf4ef9f36ade904e6474d77445138b298b92
Kenechukwu Akubue <kengoon19@gmail.com>
2024-04-09 05:05:50 +0100

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const multiLine = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < multiLine.length; i++) {
+  console.log(multiLine[i]);
+}
