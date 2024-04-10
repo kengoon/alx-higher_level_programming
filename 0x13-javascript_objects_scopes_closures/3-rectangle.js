c9f7b2d5c22478145377b1713eaa3497f2ac4010
Kenechukwu Akubue <kengoon19@gmail.com>
2024-04-10 09:07:27 +0100

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,6 +5,7 @@ module.exports = class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) 
+      console.log('X'.repeat(this.width));
   }
 };
