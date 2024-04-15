c42dd107a82afe6a3905e423bf7867e1a9781373
Kenechukwu Akubue <kengoon19@gmail.com>
2024-04-15 19:56:42 +0100

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-a script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument.
+a script that takes in an argument
 """
+
 from MySQLdb import connect
 from sys import argv
 
@@ -10,22 +10,10 @@ from sys import argv
 def main(user, password, db_name, state_name):
     """
     main function
-    :param user:
-    :param password:
-    :param db_name:
-    :param state_name:
-    :return:
     """
     db = connect(host="localhost", user=user, passwd=password, db=db_name)
     cur = db.cursor()
     cur.execute(f"SELECT * FROM states WHERE name = '{state_name}'")
-    rows = cur.fetchall()
-    for row in rows:
+
+    for row in cur.fetchall():
         print(row)
-    cur.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    argv = argv[1:]
-    main(*argv)
