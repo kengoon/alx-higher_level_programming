debfb4229e4ff36d9e3985e9c0a51e6c4021f7a8
Kenechukwu Akubue <kengoon19@gmail.com>
2024-04-15 22:27:48 +0100

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,34 +1,42 @@
 #!/usr/bin/python3
 """
-Write a script that takes in an argument
-and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
-"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
 
+This time the script is safe from
+MySQL injections!
+"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
-    conn_db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    cursor = conn_db.cursor()
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY
+                states.id ASC
+        """, {
+            'name': argv[4]
+        })
 
-    forth_arg = argv[4]
-    query_l = "SELECT * FROM states WHERE name = %s"
+        rows = cur.fetchall()
 
-    cursor.execute(query_l, (forth_arg,))
-
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-
-    cursor.close()
-    conn_db.close()
+    if rows is not None:
+        for row in rows:
+            print(row)
