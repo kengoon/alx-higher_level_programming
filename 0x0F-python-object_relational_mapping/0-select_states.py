b4b05ef8f7dd221f90b2af58c3d48b5642e92894
Kenechukwu Akubue <kengoon19@gmail.com>
2024-04-15 17:14:50 +0100

    Select states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,36 @@
+#!/bin/usr/python3
+"""
+MySQL script that lists all states from the database hbtn_0d_usa.
+"""
+from MySQLdb import connect
+from sys import argv
+
+
+def main(username, password, database):
+    """
+    Main function.
+    :param username:
+    :param password:
+    :param database:
+    :return:
+    """
+    conn = connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM states")
+    rows = cur.fetchall()
+    for id_, name in rows:
+        print(f"({id_}, '{name}')")
+
+
+if __name__ == "__main__":
+    argv = argv[1:]
+    if len(argv) != 3:
+        print(f"Usage: {argv[0]} username password database")
+        exit(1)
+    main(*argv)
