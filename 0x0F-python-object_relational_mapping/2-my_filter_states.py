58935d6ed8100c7c3a3fb341832f820fd96e7af0
Kenechukwu Akubue <kengoon19@gmail.com>
2024-04-15 21:31:07 +0100

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
 """
-a script that takes in an argument
+Write a script that takes in an argument
+and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
 """
 
-from MySQLdb import connect
-from sys import argv
 
+import sys
+import MySQLdb
 
-def main(user, password, db_name, state_name):
-    """
-    main function
-    """
-    db = connect(host="localhost", user=user, passwd=password, db=db_name)
-    cur = db.cursor()
-    cur.execute(f"SELECT * FROM states WHERE name = '{state_name}'")
+if __name__ == '__main__':
 
-    for row in cur.fetchall():
+    conn_db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = conn_db.cursor()
+
+    forth_arg = sys.argv[4]
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}%'".format(forth_arg)
+        )
+
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
 
-
-if __name__ == "__main__":
-    main(*argv[1:])
+    cursor.close()
+    conn_db.close()
