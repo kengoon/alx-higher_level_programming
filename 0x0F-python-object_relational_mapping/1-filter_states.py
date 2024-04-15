9868f08724cf4a00f0cff0e13f5b32278be62fd9
Kenechukwu Akubue <kengoon19@gmail.com>
2024-04-15 17:33:14 +0100

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+MySQL script that lists all states that starts with
+the letter N in the table states
+"""
+from sys import argv
+
+from MySQLdb import connect
+
+
+def main(username, password, database):
+    """
+    Main function.
+    :param username:
+    :param password:
+    :param database:
+    :return:
+    """
+    conn = connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%'")
+    rows = cur.fetchall()
+    for id_, name in rows:
+        print(f"({id_}, '{name}')")
+
+
+if __name__ == "__main__":
+    argv = argv[1:]
+    if len(argv) != 3:
+        print(f"Usage: {argv[0]} username password database")
+        exit(1)
+    main(*argv)
